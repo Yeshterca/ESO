bb2644cec399e7ca0cebcab29dd6ecbe5eddb55b
Yeshterca <kajina0351@gmail.com>
2024-07-24 12:37:51 +0200

    cleaned code main ICA

--- General/SVMC/main_SVMC_ICA.py
+++ General/SVMC/main_SVMC_ICA.py
@@ -4,7 +4,9 @@ from ica_run import *
 dataset = 'ikem'
 print(dataset)
 no_subjects_ikem, no_hc_ikem, no_fes_ikem, relevant_ics_ikem, no_features_ica_ikem, directory_ica_ikem, path_ikem = set_vars(dataset)
-features_ikem, labels_ikem, weights_ikem, features_or_ikem = ica_load_prep_data(directory_ica_ikem, no_subjects_ikem, no_hc_ikem, no_fes_ikem, relevant_ics_ikem, no_features_ica_ikem)
+features_ikem, labels_ikem, weights_ikem, features_or_ikem = ica_load_prep_data(directory_ica_ikem, no_subjects_ikem,
+                                                                                no_hc_ikem, no_fes_ikem, relevant_ics_ikem,
+                                                                                no_features_ica_ikem)
 acc_ica, sen_ica, spec_ica = ica_classify(features_ikem, labels_ikem, weights_ikem, 'linear')
 
 # plot the distribution of beta values for selected ICs
@@ -15,7 +17,9 @@ dataset = 'nudz'
 print('\n')
 print(dataset)
 no_subjects_nudz, no_hc_nudz, no_fes_nudz, relevant_ics_nudz, no_features_ica_nudz, directory_ica_nudz, path_nudz = set_vars(dataset)
-features_nudz, labels_nudz, weights_nudz, features_or_nudz = ica_load_prep_data(directory_ica_nudz, no_subjects_nudz, no_hc_nudz, no_fes_nudz, relevant_ics_nudz, no_features_ica_nudz)
+features_nudz, labels_nudz, weights_nudz, features_or_nudz = ica_load_prep_data(directory_ica_nudz, no_subjects_nudz,
+                                                                                no_hc_nudz, no_fes_nudz, relevant_ics_nudz,
+                                                                                no_features_ica_nudz)
 acc_ica, sen_ica, spec_ica = ica_classify(features_nudz, labels_nudz, weights_nudz, 'linear')
 
 # plot the distribution of beta values for selected ICs
@@ -26,8 +30,9 @@ acc_ica, sen_ica, spec_ica = ica_classify(features_nudz, labels_nudz, weights_nu
 
 
 ### CLASSIFY NUDZ USING IKEM, ICA COMPUTED ON MERGED DATASETS
-#no_subjects_nudz, no_hc_nudz, no_fes_nudz, relevant_ics_nudz, no_features_ica_nudz, directory_ica_nudz, path_merged = set_vars('merged')
-#ica_features, labels, weights, features_or_merged = ica_load_prep_data(directory_ica_nudz, no_subjects_nudz, no_hc_nudz, no_fes_nudz, relevant_ics_nudz, no_features_ica_nudz)
+no_subjects_nudz, no_hc_nudz, no_fes_nudz, relevant_ics_nudz, no_features_ica_nudz, directory_ica_nudz, path_merged = set_vars('merged')
+ica_features, labels, weights, features_or_merged = ica_load_prep_data(directory_ica_nudz, no_subjects_nudz, no_hc_nudz,
+                                                                       no_fes_nudz, relevant_ics_nudz, no_features_ica_nudz)
 
 # plot distributions of beta values for selected ICs
 #mfeatures_ikem = ica_features[0:131, :]
@@ -44,12 +49,14 @@ acc_ica, sen_ica, spec_ica = ica_classify(features_nudz, labels_nudz, weights_nu
 #betas_distributions_c(mfeatures_ikem, relevant_ics_nudz, path_merged_ikem, no_hc_mikem)
 #betas_distributions_c(mfeatures_nudz, relevant_ics_nudz, path_merged_nudz, no_hc_mnudz)
 
-# # shuffle subjects
-# labels_s_ikem, weights_s_ikem, ica_features_s_ikem = shuffle_subjects(labels_ikem, weights_ikem, ica_features[0:no_subjects_ikem, :])
-# labels_s_nudz, weights_s_nudz, ica_features_s_nudz = shuffle_subjects(labels_nudz, weights_nudz, ica_features[no_subjects_ikem:, :])
-#
-# print('\n')
-# print('train on IKEM, classify NUDZ')
-# accuracy, sensitivity, specificity = ica_classify_otherdataset(ica_features_s_ikem, labels_s_ikem, weights_s_ikem, ica_features_s_nudz, labels_s_nudz, weights_s_nudz, 'linear')
+# shuffle subjects
+labels_s_ikem, weights_s_ikem, ica_features_s_ikem = shuffle_subjects(labels_ikem, weights_ikem, ica_features[0:no_subjects_ikem, :])
+labels_s_nudz, weights_s_nudz, ica_features_s_nudz = shuffle_subjects(labels_nudz, weights_nudz, ica_features[no_subjects_ikem:, :])
+
+print('\n')
+print('train on IKEM, classify NUDZ')
+accuracy, sensitivity, specificity = ica_classify_otherdataset(ica_features_s_ikem, labels_s_ikem,
+                                                               weights_s_ikem, ica_features_s_nudz,
+                                                               labels_s_nudz, weights_s_nudz, 'linear')
 
 
