f0e27e466bfbacf1327bc091ca18d26477d24431
Yeshterca <kajina0351@gmail.com>
2024-07-31 14:59:29 +0200

    perform linear regression and pca on data clustered with craddock

--- General/Clustering/cluster_craddock.py
+++ General/Clustering/cluster_craddock.py
@@ -2,6 +2,10 @@ from nilearn import datasets, image, input_data, plotting
 import nibabel as nib
 import os
 import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+import h5py
 
 
 def load_atlas(clusters_slice, parc_type):
@@ -26,19 +30,62 @@ def get_rois(masker, dirlist):
             file = r'{}'.format(file)
             if i == 0:
                 time_series = masker.fit_transform(file)
-            else:
                 time_series_all = np.zeros((time_series.shape[0], time_series.shape[1], no_subjects))
                 time_series_all[:, :, 0] = time_series
+            else:
                 time_series_all[:, :, i] = masker.fit_transform(file)
             i += 1
 
     return time_series_all
 
 
+def compute_regression(time_experiment, time_subject):
+
+    model = LinearRegression()
+    model.fit(time_experiment, time_subject)
+
+    coefficients = model.coef_
+
+    return coefficients
+
+
+def get_betas(time_series, time_experiment):
+
+    no_subjects = np.shape(time_series)[2]
+    no_clusters = np.shape(time_series)[1]
+
+    coeffs = np.zeros((no_subjects, no_clusters))
+    for i in range(no_subjects):
+        for j in range(no_clusters):
+            coeffs[i, j] = compute_regression(time_experiment.reshape(-1, 1), time_series[:, j, i])  # single feature
+                # experiment = X, activity = y
+
+    return coeffs
+
+
+def perform_pca(coeffs, no_components):
+
+    #data = StandardScaler().fit_transform(coeffs)
+
+    pca = PCA(no_components)
+    components = pca.fit_transform(coeffs)
+
+    return components
+
+
 dirlist = r'C:\Users\kajin\PhD\zkouska.txt'
+SPM_mat = h5py.File(r'C:\Users\kajin\PhD\ESO\SPM.mat', 'r')
+time_experiment = SPM_mat['spmVar/xX/X'][0, :]
 
 masker = load_atlas(20, 'tcorr_mean')
 #time_series_all = masker.fit_transform(r'C:\Users\kajin\PhD\swauJOYSTICK_30_3x3x3_20150713151754_3.nii')
 time_series = get_rois(masker, dirlist)
+
+coeffs = get_betas(time_series, time_experiment)
+print(np.shape(coeffs))
+
+components = perform_pca(coeffs, 2)
+
+print(components)
 
 
