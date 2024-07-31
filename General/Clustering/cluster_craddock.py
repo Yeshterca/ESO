d295b6de0d6765c1499aed14bfaa455f8a7e015d
Yeshterca <kajina0351@gmail.com>
2024-07-31 11:26:30 +0200

    cluster all subjects

--- General/Clustering/cluster_craddock.py
+++ General/Clustering/cluster_craddock.py
@@ -1,19 +1,44 @@
 from nilearn import datasets, image, input_data, plotting
 import nibabel as nib
+import os
+import numpy as np
 
-# Fetch Craddock atlas, use temporal correlation
-craddock_atlas = datasets.fetch_atlas_craddock_2012()['tcorr_mean']
-craddock = image.index_img(craddock_atlas, 20)  # load slice with the result for 200 clusters
 
-# Load fMRI data
-fmri_img = image.load_img(r'C:\Users\kajin\PhD\swauJOYSTICK_30_3x3x3_20150713151754_3.nii')
+def load_atlas(clusters_slice, parc_type):
 
-# Create the masker object
-masker = input_data.NiftiLabelsMasker(labels_img=craddock, standardize=True, memory='nilearn_cache')
-plotting.plot_roi(craddock)
+    atlas = datasets.fetch_atlas_craddock_2012()[parc_type]
+    parcellation = image.index_img(atlas, clusters_slice)
 
-# Get time-series for each ROI
-time_series = masker.fit_transform(r'C:\Users\kajin\PhD\swauJOYSTICK_30_3x3x3_20150713151754_3.nii')
+    masker = input_data.NiftiLabelsMasker(labels_img=parcellation, standardize=True, memory='nilearn_cache')
+    #plotting.plot_roi(parcellation)
 
-print(time_series.shape)
+    return masker
+
+
+def get_rois(masker, dirlist):
+
+    i = 0
+    with open(dirlist, 'r') as file:
+        subjects = file.readlines()
+        no_subjects = len(subjects)
+        for file in subjects:
+            file = file.strip()
+            file = r'{}'.format(file)
+            if i == 0:
+                time_series = masker.fit_transform(file)
+            else:
+                time_series_all = np.zeros((time_series.shape[0], time_series.shape[1], no_subjects))
+                time_series_all[:, :, 0] = time_series
+                time_series_all[:, :, i] = masker.fit_transform(file)
+            i += 1
+
+    return time_series_all
+
+
+dirlist = r'C:\Users\kajin\PhD\zkouska.txt'
+
+masker = load_atlas(20, 'tcorr_mean')
+#time_series_all = masker.fit_transform(r'C:\Users\kajin\PhD\swauJOYSTICK_30_3x3x3_20150713151754_3.nii')
+time_series = get_rois(masker, dirlist)
+
 
